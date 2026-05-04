a6a4cd0a16b949fe4d489ef3c834188616640435
Harlockman04 <b.ambuman98@gmail.com>
2026-05-03 21:37:33 -0400

    Actualizar script.js

--- frontend/script.js
+++ frontend/script.js
@@ -1,4 +1,4 @@
-const API_URL = 'https://tu-app-en-render.onrender.com'; // Cambia por tu URL de Render
+const API_URL = 'https://learnpy.onrender.com'; // Cambia por tu URL de Render
 let userId = 'user_' + Date.now() + '_' + Math.random().toString(36).substr(2, 9);
 let currentExercise = '';
 let currentLevel = 'basico';
